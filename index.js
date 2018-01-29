c361cd5a34d6537a8e1c7ad4f3049a1b9499789e
Tiago Britto Lobão <tiago.blobao@gmail.com>
2018-01-28 23:11:13 -0300

    Ajustes Back-end pagina "mover"

--- index.js
+++ index.js
@@ -14,6 +14,36 @@ app.get('/', function(req,res){
 /**** Pages Declarations ****/
 app.get('/mover', function(req,res){
 	res.render("mover");
+	var comando = req.query.command;
+	console.log(req.query.command);
+	//Se existe alguma entrada de dados
+	if(typeof comando !== 'undefined'){
+		//Configuração do shell exec
+		var execOptions = {
+			async: true,
+			silent: false,
+			encoding: 'utf8'
+		};
+		//Escolha do comando
+		var output = "./scriptsCpp/teste a";
+		switch (comando) {
+			case 'esquerda':
+				output += "e";
+				break;
+			case 'direita':
+				output += "d";
+				break;
+			case 'frente':
+				output += "f";
+				break;
+			case 'parar':
+				output += "p";
+				break;
+		}
+		shell.exec(output,execOptions,function(){
+			//CALLBACK
+		});
+	}
 })
 
 app.get('/logo.png', function(req,res){
